295fdb6559a0b679b7793c08db9a55c2e7726379
Hiago Henrique <hiago_h3@hotmail.com>
2025-01-09 18:48:16 -0300

    Projeto concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("")
+  const [copy, setCopy] = useState("Copiar")
+
+  function generate(){
+    let characters = "'123456789/*-+qwertyuiop´[]~çlkjhgfdsa\zxcvbnm,.;|?'"
+    const length = 12;
+    let newPassword = "";
+    for (let i = 0; i < length; i++) {
+      const randomPosition = Math.floor(Math.random() * characters.length)
+      newPassword+=characters[randomPosition]
+    }
+    setPassword(newPassword)
+    setCopy("Copiar")
+  }
+  
+ //Para copiar para a área de transferencia 
+function copyFunction(){
+  setCopy("Copiado")
+  window.navigator.clipboard.writeText(password)
+}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Gerador de senhas</h1>
+      <button onClick={generate}>Gerar!</button>
+      <button onClick={copyFunction}>{copy}</button>
+      <div>{password}</div>
     </>
   )
 }
