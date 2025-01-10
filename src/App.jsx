9cbec4ba52ccd9529d53e405f66dc67c62aa8cea
Hiago Henrique <hiago_h3@hotmail.com>
2025-01-10 16:16:55 -0300

    Adição de input
    Adição de um input para que o usuário consiga dizer com quantos caracteres ele deseja a senha.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { useState } from "react"
 function App() {
   const [password, setPassword] = useState("")
   const [copy, setCopy] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState();
 
   function generate(){
     let characters = "'123456789/*-+qwertyuiop´[]~çlkjhgfdsa\zxcvbnm,.;|?'"
-    const length = 12;
+    const length = passwordSize;
     let newPassword = "";
     for (let i = 0; i < length; i++) {
       const randomPosition = Math.floor(Math.random() * characters.length)
@@ -25,6 +26,18 @@ function copyFunction(){
   return (
     <>
       <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input 
+          type="number" 
+          id="passwordSize"
+          placeholder="Quantidade de caracteres da senha" 
+          min={1}
+          value={passwordSize}
+          onChange={(ev)=> setPasswordSize(ev.target.value)}
+        />
+
+      </div>
       <button onClick={generate}>Gerar!</button>
       <button onClick={copyFunction}>{copy}</button>
       <div>{password}</div>
